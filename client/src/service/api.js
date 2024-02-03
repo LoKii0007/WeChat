92f3f7920c74bacccc2b858edaf811da84434809
Lokii0007 <lokeshyadav1098@gmail.com>
2024-02-03 20:45:20 +0530

    vercel

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const url = "http://localhost:8000"
+// const url = "http://localhost:8000"
+const url = "https://we-chat-ten.vercel.app"
 
 export const adduser = async(data) =>{
     try{
